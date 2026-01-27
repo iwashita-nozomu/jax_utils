5ad90ee843517bd501ab882887994f455b14387c
niwashita <niwashita@users.noreply.github.com>
2026-01-27 22:43:07 +0900

    update:git initilatizer

---
+++ Makefile
@@ -0,0 +1,2 @@
+git_init:
+	./scripts/git_init.sh
---
+++ scripts/git_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+username="niwashita"
+
+git config user.name "${username}"
+git config user.email "${username}@users.noreply.github.com"
